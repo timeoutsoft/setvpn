267af90e63411e5340c203e228c78dd5a8a638f0
varchar <varchar@list.ru>
2024-11-14 00:04:52 +0300

    after test friday evening

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import secrets
 import string
 
 
+author_info="""
+*****
+Disclaimer:
+***** 
+Eta proga mozhet rabotat ne tak, kak khotelos.
+Poka server zhiv, mozhno nazhat CTRL+C
+I etot script ostavovitsa.
+*****
+
+******
+It's test script for our "GreatIdea" 'bout free VPN for BDO guild 'TimeOut' mmbrz
+Thnx to Zlo101 and Sanych. They're Inspired me :)    
+Wrote by Shirker aka Progulschik
+******
+"""
+print(author_info)
 ipsec_secrets_text="""
 : RSA "server-key.pem"
 """
@@ -48,12 +64,12 @@ ufw_rule1="""
 
 #added by vpnset
 *nat
--A POSTROUTING -s 10.10.10.0/24 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
--A POSTROUTING -s 10.10.10.0/24 -o eth0 -j MASQUERADE
+-A POSTROUTING -s 192.168.0.0/16 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
+-A POSTROUTING -s 192.168.0.0/16 -o eth0 -j MASQUERADE
 COMMIT
 
 *mangle
--A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.0/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+-A FORWARD --match policy --pol ipsec --dir in -s 192.168.0.0/16 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 COMMIT
 
 
@@ -62,14 +78,14 @@ COMMIT
 ufw_rule2=""" 
 
 #added by vpnset
--A ufw-before-forward --match policy --pol ipsec --dir in --proto esp -s 10.10.10.0/24 -j ACCEPT
--A ufw-before-forward --match policy --pol ipsec --dir out --proto esp -d 10.10.10.0/24 -j ACCEPT
+-A ufw-before-forward --match policy --pol ipsec --dir in --proto esp -s 192.168.0.0/16 -j ACCEPT
+-A ufw-before-forward --match policy --pol ipsec --dir out --proto esp -d 192.168.0.0/16 -j ACCEPT
 
 """
 
 def find_ip ():
     import urllib.request
-    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    external_ip = urllib.request.urlopen('https://v4.ident.me').read().decode('utf8')
     return external_ip
 
 def generate_username():
@@ -85,8 +101,8 @@ def generate_password():
 
 
 def set_before_rules():
-    print("Starting to modify before.rules.")
-    path = r"before.rules"
+    print("Starting to modify before.rules")
+    path = r"./before.rules"
 
     # Each line of the file is read, and
     # stored in a list variable
@@ -99,7 +115,6 @@ def set_before_rules():
             print("File before.rules already modified by vpnset. Operation canceled!")
             return
 
-
         if x=='# Don\'t delete these required lines, otherwise there will be errors\n':
             text = text+ ufw_rule1 +x
         elif  x=='# allow all on loopback\n':
@@ -107,14 +122,14 @@ def set_before_rules():
         else:
             text = text+ x
 
-    os.rename('before.rules', 'before.rules.orig')
-    with open('before.rules', 'a') as f:
+    #os.rename('before.rules', 'before.rules.orig')
+    with open('./before.rules', 'w') as f:
         f.write(text)
     print("File before.rules modified.")
 
 def set_sysctl_conf():
     print("Starting to modify sysctl.conf.")
-    path = r"sysctl.conf"
+    path = r"./sysctl.conf"
 
     data = open(path, 'r').readlines()
 
@@ -132,8 +147,8 @@ def set_sysctl_conf():
             text = text+ x
 
     text=text+"\n#added by vpnset\nnet/ipv4/ip_no_pmtu_disc=1\n"
-    os.rename('sysctl.conf', 'sysctl.conf.orig')
-    with open('sysctl.conf', 'w') as f:
+    #os.rename('sysctl.conf', 'sysctl.conf.orig')
+    with open('./sysctl.conf', 'w') as f:
         f.write(text)
     print("File sysctl.conf modified.")
 
@@ -154,16 +169,22 @@ def set_ipsec_secrets(u1n, u1p, u2n, u2p, u3n, u3p):
     text = text+ u2n + ':"' + u2p + '"\n'
     text = text + u3n + ':"' + u3p + '"\n'
 
-    with open('ipsec.secrets', 'w') as f:
+    with open('./ipsec.secrets', 'w') as f:
         f.write(text)
     #print (text)
     print("File ipsec.secrets created.")
 
 def copy_source_files():
-    os.system('cp -f /etc/ipsec.conf ./**')
-    os.system('cp -f /etc/ipsec.secrests ./**')
-    os.system('cp -f /etc/ufw/before.rules ./**')
-    os.system('cp -f /etc/ufw/sysctl.conf ./**')
+    os.system('cp -f /etc/ipsec.conf ./')
+    os.system('cp -f /etc/ipsec.secrets ./')
+    os.system('cp -f /etc/ufw/before.rules ./')
+    os.system('cp -f /etc/ufw/sysctl.conf ./')
+
+def back_copy_source_files():
+    os.system('cp -f ./ipsec.conf /etc/ipsec.conf')
+    os.system('cp -f ./ipsec.secrets /etc/ipsec.secrets ')
+    os.system('cp -f ./before.rules /etc/ufw/before.rules ')
+    os.system('cp -f ./sysctl.conf  /etc/ufw/sysctl.conf ')
 
 
 def os_pre_operations(root_ca_name='TimeOutSoft CA', server_dn_ip='127.0.0.1'):
@@ -174,15 +195,22 @@ def os_pre_operations(root_ca_name='TimeOutSoft CA', server_dn_ip='127.0.0.1'):
     #pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem
     #pki --pub --in ~/pki/private/server-key.pem --type rsa | pki --issue --lifetime 1825  --cacert ~/pki/cacerts/ca-cert.pem  --cakey ~/pki/private/ca-key.pem --dn "CN=91.149.253.28" --san 91.149.253.28  --flag serverAuth --flag ikeIntermediate --outform pem  > ~/pki/certs/server-cert.pem
     #cp -r ~/pki/* /etc/ipsec.d/
-
-    os.system('mkdir -p ~/pki/{cacerts,certs,private}')
-    os.system('chmod 700 ~/pki')
-    os.system('pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem')
-    os.system('pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem --type rsa --dn "CN='+root_ca_name.strip()+'" --outform pem > ~/pki/cacerts/ca-cert.pem')
-    os.system('pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem')
-    os.system('pki --pub --in ~/pki/private/server-key.pem --type rsa | pki --issue --lifetime 1825  --cacert ~/pki/cacerts/ca-cert.pem  --cakey ~/pki/private/ca-key.pem --dn "CN='+server_dn_ip+'" --san '+server_dn_ip+'  --flag serverAuth --flag ikeIntermediate --outform pem  > ~/pki/certs/server-cert.pem')
-    os.system('cp -r ~/pki/* /etc/ipsec.d/')
-    os.system('cat /etc/ipsec.d/cacerts/ca-cert.pem')
+    print("Creating Certs")
+    os.system('mkdir -p ./pki/cacerts')
+    os.system('mkdir -p ./pki/certs')
+    os.system('mkdir -p ./pki/private')
+    os.system('chmod 700 ./pki')
+    print("Creating CA Key")
+    os.system('pki --gen --type rsa --size 4096 --outform pem > ./pki/private/ca-key.pem')
+    os.system('pki --self --ca --lifetime 3650 --in ./pki/private/ca-key.pem --type rsa --dn "CN='+root_ca_name.strip()+'" --outform pem > ./pki/cacerts/ca-cert.pem')
+    print("Creating Server Key")
+    os.system('pki --gen --type rsa --size 4096 --outform pem > ./pki/private/server-key.pem')
+    os.system('pki --pub --in ./pki/private/server-key.pem --type rsa | pki --issue --lifetime 1825  --cacert ./pki/cacerts/ca-cert.pem  --cakey ./pki/private/ca-key.pem --dn "CN='+server_dn_ip+'" --san '+server_dn_ip+'  --flag serverAuth --flag ikeIntermediate --outform pem  > ./pki/certs/server-cert.pem')
+    print("Copyng keys into /etc")
+    os.system('cp -r ./pki/* /etc/ipsec.d/')
+    os.system('cat /etc/ipsec.d/cacerts/ca-cert.pem >yourCAroot.crt')
+    print("It's your cert:")
+    os.system('cat /etc/ipsec.d/cacerts/ca-cert.pem ')
 
     return True
 
@@ -210,8 +238,15 @@ if __name__ == '__main__':
 
 
     input("Press a key to continue")
-    os_pre_operations()
-    #set_before_rules()
-    #set_sysctl_conf()
-    #set_ipsec_conf(srv_ip=server_ip, clnt_subn=clients_subnet)
-    #set_ipsec_secrets(un1, pw1, un2, pw2, un3, pw3)
+    root_ca_name_main="TimeOutSoft CA"
+    server_dn_ip_main = server_ip
+
+    os_pre_operations(root_ca_name=root_ca_name_main, server_dn_ip=server_dn_ip_main)
+    print("Copying config files")
+    copy_source_files()
+    print("Starting changes")
+    set_before_rules()
+    set_sysctl_conf()
+    set_ipsec_conf(srv_ip=server_ip, clnt_subn=clients_subnet)
+    set_ipsec_secrets(un1, pw1, un2, pw2, un3, pw3)
+    back_copy_source_files()
